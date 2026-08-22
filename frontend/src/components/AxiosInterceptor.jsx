91ed85b74e0f7b04616ffad817277415df0e55a4
ronak prajapati <ronak03p@gmail.com>
2026-08-22 21:25:12 +0530

    fix Clerk authentication

---
+++ frontend/src/components/AxiosInterceptor.jsx
@@ -0,0 +1,29 @@
+import { useAuth } from "@clerk/clerk-react";
+import { useEffect } from "react";
+import axiosInstance from "../lib/axios";
+
+const AxiosInterceptor = ({ children }) => {
+  const { getToken } = useAuth();
+
+  useEffect(() => {
+    const interceptor = axiosInstance.interceptors.request.use(
+      async (config) => {
+        const token = await getToken();
+
+        if (token) {
+          config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+      }
+    );
+
+    return () => {
+      axiosInstance.interceptors.request.eject(interceptor);
+    };
+  }, [getToken]);
+
+  return children;
+};
+
+export default AxiosInterceptor;
