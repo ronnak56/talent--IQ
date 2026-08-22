5165ea6b090c3fce1f9ea87234bb2b8a86d58c3e
ronak prajapati <ronak03p@gmail.com>
2026-08-22 21:33:13 +0530

    fix Clerk authentication

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,22 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true, // by adding this field browser will send the cookies to server automatically, on every single req
+  withCredentials: true,
 });
+
+export const setupAxiosInterceptors = (getToken) => {
+  axiosInstance.interceptors.request.use(
+    async (config) => {
+      const token = await getToken();
+
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+
+      return config;
+    },
+    (error) => Promise.reject(error)
+  );
+};
 
 export default axiosInstance;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+
 import App from "./App.jsx";
 
 import { ClerkProvider } from "@clerk/clerk-react";
@@ -9,7 +10,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import AxiosInterceptor from "./components/AxiosInterceptor.jsx";
 
-// Import your Publishable Key
+// Clerk Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
