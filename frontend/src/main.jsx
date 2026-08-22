b94caf23c09b391fdc48ab58ef900ae735131d3c
ronak prajapati <ronak03p@gmail.com>
2026-08-22 21:27:16 +0530

    fix Clerk authentication

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+
 import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import AxiosInterceptor from "./components/AxiosInterceptor.jsx";
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -20,7 +23,9 @@ createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-          <App />
+          <AxiosInterceptor>
+            <App />
+          </AxiosInterceptor>
         </ClerkProvider>
       </QueryClientProvider>
     </BrowserRouter>
