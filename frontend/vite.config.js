f4259c278a27e7fc8cee2e3d8ec9a0162d896577
ronak prajapati <ronak03p@gmail.com>
2026-08-22 16:31:05 +0530

    Fix Tailwind CSS configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+
   preview: {
-    allowedHosts: ['talent-iq-1-148b.onrender.com'],
+    allowedHosts: ["talent-iq-1-148b.onrender.com"],
   },
-})
+});
