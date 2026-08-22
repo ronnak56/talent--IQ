07dc0c96ada859f59a4afa29f2215d0efeba3ffe
ronak prajapati <ronak03p@gmail.com>
2026-08-22 20:17:09 +0530

    fix code execution api url

--- frontend/src/lib/piston.js
+++ frontend/src/lib/piston.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/api/code/execute";
+const API_URL = `${import.meta.env.VITE_API_URL}/code`;
 
 export async function executeCode(language, code, stdin = "") {
   try {
