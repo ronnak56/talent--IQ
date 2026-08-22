04a10ee1092d0258ad39949964e7be030a8e7467
ronak prajapati <ronak03p@gmail.com>
2026-08-22 20:35:33 +0530

    fix stream configuration

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -9,23 +9,48 @@ if (!apiKey || !apiSecret) {
   console.error("STREAM_API_KEY or STREAM_API_SECRET is missing");
 }
 
-export const chatClient = StreamChat.getInstance(apiKey, apiSecret); // will be used chat features
-export const streamClient = new StreamClient(apiKey, apiSecret); // will be used for video calls
+export const chatClient = StreamChat.getInstance(
+  apiKey,
+  apiSecret
+);
+
+export const streamClient = new StreamClient(
+  apiKey,
+  apiSecret
+);
 
 export const upsertStreamUser = async (userData) => {
   try {
     await chatClient.upsertUser(userData);
-    console.log("Stream user upserted successfully:", userData);
+
+    console.log(
+      "Stream user upserted successfully:",
+      userData
+    );
   } catch (error) {
-    console.error("Error upserting Stream user:", error);
+    console.error(
+      "Error upserting Stream user:",
+      error
+    );
+
+    throw error;
   }
 };
 
 export const deleteStreamUser = async (userId) => {
   try {
     await chatClient.deleteUser(userId);
-    console.log("Stream user deleted successfully:", userId);
+
+    console.log(
+      "Stream user deleted successfully:",
+      userId
+    );
   } catch (error) {
-    console.error("Error deleting the Stream user:", error);
+    console.error(
+      "Error deleting the Stream user:",
+      error
+    );
+
+    throw error;
   }
 };
